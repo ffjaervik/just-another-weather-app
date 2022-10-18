3d22c44423c0ce96807a09042c2072e8cd7a922f
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-18 16:53:59 +0100

    mack data and page layout complete

--- another-weather-app/src/App.js
+++ another-weather-app/src/App.js
@@ -1,50 +1,36 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+	//const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=61f73456f424d0ad090e0e62da2168da`
 
-  const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState(null);
-  const [country, setCountry] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (city && country) {
-      setLoading(true);
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
-          setWeather(data);
-          setLoading(false);
-        })
-        .catch(err => {
-          setError(err);
-          setLoading(false);
-        })
-    }
-  }, [city, country]);
-
-
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <div className="weather">
-        <div className="weather__form">
-          <input type="text" placeholder="City" onChange={e => setCity(e.target.value)} />
-          <input type="text" placeholder="Country" onChange={e => setCountry(e.target.value)} />
-        </div>
-        {loading && <p>Loading...</p>}
-        {weather && (
-          <div className="weather__data">
-            <p>{weather.name}, {weather.sys.country}</p>
-            <p>{weather.weather[0].main}</p>
-            <p>{weather.main.temp}°F</p>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className='Container'>
+			<div className='container'>
+				<div className='top'>
+					<div className='location'>
+						<p>London</p>
+					</div>
+					<div className='temp'>
+						<h1>60ºC</h1>
+					</div>
+					<div className='description'>
+						<p>Clouds</p>
+					</div>
+				</div>
+				<div className='bottom'>
+					<div className='feels'>
+						<p>57ºC</p>
+					</div>
+					<div className='humidity'>
+						<p>20%</p>
+					</div>
+					<div className='wind'>12km/h</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
